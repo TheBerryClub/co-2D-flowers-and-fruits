d2cdb946e19d7bc2645e77d5afb3e6462e5f7d81
bluebuuurry <61054528+bluebuuurry@users.noreply.github.com>
2020-02-17 13:32:55 -0600

    added good charecter moves
    no animations yet

--- flowers and fruits/Assets/PlayerMovment.cs
+++ flowers and fruits/Assets/PlayerMovment.cs
@@ -7,15 +7,42 @@ public class PlayerMovment : MonoBehaviour
 {
 
     public CharacterController2D controller;
-    // Start is called before the first frame update
-    void Start()
+
+    public float runspeed = 40f;
+
+    bool crouch = false;
+
+    bool jump = false;
+
+    float HorizontalMove = 0f;
+        void Update()
     {
+
+        HorizontalMove = Input.GetAxisRaw("Horizontal") * runspeed;
+
+        if (Input.GetButtonDown("jump"))
+        {
+            
+            jump = true;
+            print ("jumped");
+
+        }
+
+        if(Input.GetButtonDown("crouch"))
+        {
+            crouch = true;
+
+        } else if (Input.GetButtonUp("crouch"))
+        {
+            crouch = false;
+        }
+        
         
     }
-
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate ()
     {
-        
+        //move charecter
+        controller.Move(HorizontalMove * Time.fixedDeltaTime, false, jump);
+        jump = false;
     }
 }
